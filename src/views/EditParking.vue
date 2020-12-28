204c2caffc10898678b5a4961eaeb2c6420615d1
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-28 19:38:08 +0330

    create:editParking

---
+++ src/views/EditParking.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="edit-parking mt-3">
+    <h1 class="is-size-3 has-text-centered mt-2">ویرایش پارکینگ</h1>
+
+    <div class="container mt-2" dir="ltr" style="width: 96%">
+      <div class="columns mt-3" dir="rtl">
+        <div class="column">
+          <div class="field">
+            <label class="label">آدرس پارکینگ:</label>
+            <div class="control">
+              <input type="text" class="input" :value="address" />
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="field">
+            <label class="label">شهر:</label>
+            <div class="control">
+              <input type="text" class="input" :value="city" required />
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="field">
+            <label class="label">ظرفیت فعلی:</label>
+            <div class="control">
+              <input
+                type="number"
+                class="input"
+                :value="current_capacity"
+                required
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns mt-3" dir="rtl">
+        <div class="column">
+          <div class="field">
+            <label class="label">وضعیت:</label>
+            <div class="control">
+              <input type="text" class="input" :value="persian_status" />
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="field">
+            <label class="label">انتخاب فایل:</label>
+            <div class="control">
+              <div class="file">
+                <label class="file-label">
+                  <input class="file-input" type="file" name="resume" />
+                  <span class="file-cta">
+                    <span class="file-icon">
+                      <i class="fa fa-upload"></i>
+                    </span>
+                    <span class="file-label"> Choose a file… </span>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p class="control mb-5">
+        <button class="button mr-3 is-success">ذخیره</button>
+        <button class="button is-danger">لغو</button>
+      </p>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditParking",
+  data() {
+    return {
+      address: "",
+      city: "",
+      current_capacity: "",
+      persian_status: "",
+      file: "",
+    };
+  },
+  mounted() {
+    this.address = this.$route.query.address;
+    this.city = this.$route.query.city;
+    this.current_capacity = this.$route.query.current_capacity;
+    this.persian_status = this.$route.query.persian_status;
+    this.file = this.$route.query.file;
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Parking.vue
+++ src/views/Parking.vue
@@ -1,73 +1,85 @@
 <template>
   <div class="parking-page mt-2">
-      <Navbar />
-      <h1 class="has-text-centered is-size-3 mb-4"> لیست همه پارکینگ ها </h1>
+    <Navbar />
+    <h1 class="has-text-centered is-size-3 mb-4">لیست همه پارکینگ ها</h1>
 
     <div class="container" dir="rtl">
-        <div class="columns is-multiline" v-for="(parking, index) in parkings" :key="index">
-          <div class="column is-6-tablet is-6-dekstop is-3-widescreen" v-for="(parking, index) in parkings" :key="index">
-            <div class="container mt-3" style="width: 96%">
+      <div
+        class="columns is-multiline"
+        v-for="(parking, index) in parkings"
+        :key="index"
+      >
+        <div
+          class="column is-6-tablet is-6-dekstop is-3-widescreen"
+          v-for="(parking, index) in parkings"
+          :key="index"
+        >
+          <div class="container mt-3" style="width: 96%">
 
-                <div class="box is-grey-light is-size-6 hov">
-                    <button class="delete"></button>
-                    <p>
-                        <span class="has-text-success">پارکینگ: </span>
-                        <span class="mr-3"> {{parking.address}} </span>
-                    </p>
-                    <p>
-                        <span class="has-text-success">شهر: </span>
-                        <span class="mr-3"> {{ parking.city_key}} </span>
-                    </p>
-                    <p>
-                        <span class="has-text-success">ظرفیت فعلی: </span>
-                        <span class="mr-3"> {{ parking.current_capacity}} </span>
-                    </p>
-                    <p>
-                        <span class="has-text-success">وضعیت: </span>
-                        <span class="mr-3"> {{ parking.persian_status}} </span>
-                    </p>
-                    <p>
-                        <span class="has-text-success">فایل ها: </span>
-                        <span class="mr-3"> {{ parking.persian_status_files}} </span>
-                    </p>
-                    
-                </div>
-            </div>
+            <router-link 
+                :to="{path: '/editParking', query: {address: parking.address, city: parking.city_key,
+                    current_capacity: parking.current_capacity, persian_status: parking.persian_status,
+                    file: parking.persian_status_files }}"
+            >
+              <div class="box is-grey-light is-size-6 hov">
+                <button class="delete"></button>
+                <p>
+                  <span class="has-text-success">پارکینگ: </span>
+                  <span class="mr-3"> {{ parking.address }} </span>
+                </p>
+                <p>
+                  <span class="has-text-success">شهر: </span>
+                  <span class="mr-3"> {{ parking.city_key }} </span>
+                </p>
+                <p>
+                  <span class="has-text-success">ظرفیت فعلی: </span>
+                  <span class="mr-3"> {{ parking.current_capacity }} </span>
+                </p>
+                <p>
+                  <span class="has-text-success">وضعیت: </span>
+                  <span class="mr-3"> {{ parking.persian_status }} </span>
+                </p>
+                <p>
+                  <span class="has-text-success">فایل ها: </span>
+                  <span class="mr-3"> {{ parking.persian_status_files }} </span>
+                </p>
+              </div>
+            </router-link>
+
           </div>
+        </div>
       </div>
     </div>
-      
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-    name: 'Parking',
-    components: {
-        Navbar
-    },
-    data() {
-        return {
-            parkings: []
-        }
-    },
-    mounted() {
-        this.$store.dispatch("parking")
-        .then((resp)=>{
-            this.parkings = resp.data.data
-        })
-    },
-}
+  name: "Parking",
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      parkings: [],
+    };
+  },
+  mounted() {
+    this.$store.dispatch("parking").then((resp) => {
+      this.parkings = resp.data.data;
+    });
+  },
+};
 </script>
 
 <style scoped>
-.hov:hover{
-    transform: translateY(-3px);
-    box-shadow: 0 1rem 2rem rgb(0, 0, 0, 2);
+.hov:hover {
+  transform: translateY(-3px);
+  box-shadow: 0 1rem 2rem rgb(0, 0, 0, 2);
 }
-.hov:active{
-    transform: translateY(-1px);
+.hov:active {
+  transform: translateY(-1px);
 }
 </style>
